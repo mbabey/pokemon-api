3c262494c9b77f79f088d826f112aada2aa4ccff
Maxwell Babey <babeymax@gmail.com>
2023-03-29 23:23:29 -0700

    Add fetchReport function

--- front/src/Report.js
+++ front/src/Report.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import axios from 'axios'
+import { useState, useEffect } from 'react'
 
 function Report(id) {
-  return (
-    <div>Report {id}</div>
-  )
-  }
+
+    const [reportTable, setReportTable] = React.useState(null)
+
+    useEffect(() => {
+        async function fetchReport() {
+            const res = await axios.get(`http://localhost:5000/report/${id}`);
+        }
+        fetchReport();
+    }, { id });
+
+    return (
+        <div>
+            Report {id}
+            {
+                true && <div>
+                    reportTable
+                </div>
+            }
+        </div>
+    )
+}
 
 export default Report
