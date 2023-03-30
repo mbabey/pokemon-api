59c522b7f4d0dd948851dbfa581400078b49cbfd
Maxwell Babey <babeymax@gmail.com>
2023-03-29 23:11:34 -0700

    Add Links to ul

--- front/src/Dashboard.js
+++ front/src/Dashboard.js
@@ -1,16 +1,22 @@
 import React from 'react'
 import Report from './Report'
 
+import {
+    Routes,
+    Route,
+    Link
+  } from "react-router-dom";
+
 function Dashboard() {
-  return (
-    <div>
-        <ul>
-            <li>Report 1</li>
-            <li>Report 2</li>
-            <li>Report 3</li>
-        </ul>
-    </div>
-  )
+    return (
+        <div>
+            <ul>
+                <li><Link to="/report/1">Report 1 - Unique API users over a period of time</Link></li>
+                <li><Link to="/report/2">Report 2 - Top API users over period of time</Link></li>
+                <li><Link to="/report/3">Report 3 - Top users for each Endpoint</Link></li>
+            </ul>
+        </div>
+    )
 }
 
 export default Dashboard
