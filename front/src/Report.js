14498732b3c196b4179ddf4746f777f807131054
Maxwell Babey <babeymax@gmail.com>
2023-03-29 23:44:29 -0700

    Add axios interceptors for access token renewal

--- front/src/Report.js
+++ front/src/Report.js
@@ -1,23 +1,45 @@
 import React from 'react'
 import axios from 'axios'
 import { useState, useEffect } from 'react'
+import jwt_decode from 'jwt-decode'
 
 function Report({ id, accessToken, setAccessToken, refreshToken }) {
 
     const [reportTable, setReportTable] = React.useState(null)
 
+axios.interceptors.request.use( async (config) => {
+    const decoded = jwt_decode(accessToken);
+    const currentTime = Date.now() / 1000;
+    if (decoded.exp < currentTime)
+    {
+        console.log("token expired");
+        await axios.get('http://localhost:5010/requestNewAccessToken',
+        {
+            headers: {
+                'Authorization': refreshToken
+            }
+        });
+        setAccessToken(res.headers['authorization']);
+    }
+
+    return config;
+}, (err) => {
+    return Promise.reject(err);
+});
+
     useEffect(() => {
         async function fetchReport() {
             const res = await axios.get(
-                `http://localhost:5000/report?id=${id}`,
+                `http://localhost:5010/report?id=${id}`,
                 {
                     headers: {
                         "Authorization": accessToken
                     }
                 });
+            setReportTable(res.data);
         }
         fetchReport();
-    }, { id });
+    }, [ id ]);
 
     return (
         <div>
