634b9bc29ae3fb6bc429909116f6f1a04a21e899
Maxwell Babey <babeymax@gmail.com>
2023-03-29 23:03:29 -0700

    Add conditional rendering and Dashboard.js

--- front/src/Login.js
+++ front/src/Login.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import axios from 'axios'
 import { useState } from 'react';
+import Dashboard from './Dashboard';
 
 function Login() {
 
@@ -8,40 +9,43 @@ function Login() {
     const [password, setPassword] = useState('');
     const [accessToken, setAccessToken] = useState('');
     const [refreshToken, setRefreshToken] = useState('');
-    
 
     const onClickHandle = async (e) => {
         e.preventDefault();
-        const res = await axios.post('http://localhost:5000/login', 
-        {
-            username: username,
-            password: password
-        });
+        const res = await axios.post('http://localhost:5000/login',
+            {
+                username: username,
+                password: password
+            });
         console.log(res.data);
         const authorization_tokens = res.headers('authorization').split(',');
         setAccessToken(authorization_tokens[1]);
         setRefreshToken(authorization_tokens[0]);
-        console.log(authorization_tokens);
     }
 
-  return (
-    <div>
-
-        <form onSubmit={onClickHandle}>
-            <input 
-            type="text" 
-            placeholder="username" 
-            onChange={(e) => setUsername(e.target.value)}
-            />
-            <input 
-            type="text" 
-            placeholder="password"
-            onChange={(e) => setPassword(e.target.value)}
-            />
-            <button type="submit">Login</button>
-        </form>
-    </div>
-  )
+    return (
+        <div>
+            {
+                accessToken && <Dashboard />
+            }
+            {
+                !accessToken && <form onSubmit={onClickHandle}>
+                    <input
+                        type="text"
+                        placeholder="username"
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
+                    <input
+                        type="text"
+                        placeholder="password"
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <button type="submit">Login</button>
+                </form>
+            }
+            
+        </div>
+    )
 }
 
 export default Login
