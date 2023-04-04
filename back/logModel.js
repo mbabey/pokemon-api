47db1ccc8f15de65025d66a18965dc23590122cc
Maxwell Babey <babeymax@gmail.com>
2023-04-04 00:15:39 -0700

    Add email, username, and origin to logmodel

--- back/logModel.js
+++ back/logModel.js
@@ -1,30 +1,42 @@
 const mongoose = require('mongoose');
 
 const logSchema = new mongoose.Schema({
-  timestamp: {
-    type: Number,
-    required: true
-  },
-  user_id: {
-    type: String,
-    required: true
-  },
-  endpoint: {
-    type: String,
-    required: true
-  },
-  method: {
-    type: String,
-    required: true
-  },
-  status_code: {
-    type: Number,
-    required: true
-  },
-  response_time: {
-    type: Number,
-    required: true
-  }
+    timestamp: {
+        type: Number,
+        required: true
+    },
+    user_id: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    origin: {
+        type: String,
+        required: true
+    },
+    endpoint: {
+        type: String,
+        required: true
+    },
+    method: {
+        type: String,
+        required: true
+    },
+    status_code: {
+        type: Number,
+        required: true
+    },
+    response_time: {
+        type: Number,
+        required: true
+    }
 });
 
 const Log = mongoose.model('logs', logSchema);
