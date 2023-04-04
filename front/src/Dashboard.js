c9b7d418e20c28d6065642a309700d892a4b5d0c
Maxwell Babey <babeymax@gmail.com>
2023-04-04 08:08:07 -0700

    Update report links

--- front/src/Dashboard.js
+++ front/src/Dashboard.js
@@ -14,8 +14,8 @@ function Dashboard({ accessToken, setAccessToken, refreshToken, SERVER_ADDRESS }
                 <li><Link to="/report/1">Report 1 - Unique API users over a period of time</Link></li>
                 <li><Link to="/report/2">Report 2 - Top API users over period of time</Link></li>
                 <li><Link to="/report/3">Report 3 - Top users for each Endpoint</Link></li>
-                <li><Link to="/report/3">Report 4 - 4xx Errors By Endpoint</Link></li>
-                <li><Link to="/report/3">Report 5 - Recent 4xx/5xx Errors</Link></li>
+                <li><Link to="/report/4">Report 4 - 4xx Errors By Endpoint</Link></li>
+                <li><Link to="/report/5">Report 5 - Recent 4xx/5xx Errors</Link></li>
             </ul>
 
             <Routes>
