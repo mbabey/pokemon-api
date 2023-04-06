f43da04a30d596c87ae2ee3c24a11b3de83dcc21
Maxwell Babey <babeymax@gmail.com>
2023-04-06 14:01:55 -0700

    Add styles for log report selectors

--- front/src/Dashboard.js
+++ front/src/Dashboard.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Report from './Report'
+import './styles/dashboard-styles.css'
 
 import {
     Routes,
@@ -10,20 +11,25 @@ import {
 function Dashboard({ accessToken, setAccessToken, refreshToken, SERVER_ADDRESS }) {
     return (
         <div>
-            <ul>
-                <li><Link to="/report/1">Report 1 - Unique API users over a period of time</Link></li>
-                <li><Link to="/report/2">Report 2 - Top API users over period of time</Link></li>
-                <li><Link to="/report/3">Report 3 - Top users for each Endpoint</Link></li>
-                <li><Link to="/report/4">Report 4 - 4xx Errors By Endpoint</Link></li>
-                <li><Link to="/report/5">Report 5 - Recent 4xx/5xx Errors</Link></li>
-            </ul>
+            <div className='dashboard-report-selector'>
+                <ul>
+                    <li><Link to="/report/1">Report 1 - Unique API users over a period of time</Link></li>
+                    <li><Link to="/report/2">Report 2 - Top API users over period of time</Link></li>
+                    <li><Link to="/report/3">Report 3 - Top users for each Endpoint</Link></li>
+                    <li><Link to="/report/4">Report 4 - 4xx Errors By Endpoint</Link></li>
+                    <li><Link to="/report/5">Report 5 - Recent 4xx/5xx Errors</Link></li>
+                </ul>
+                <div className='popout-clicker'>
+                    <div>. . .</div>
+                </div>
+            </div>
 
             <Routes>
-                <Route path="/report/1" element={<Report id={1} accessToken={accessToken} setAccessToken={setAccessToken} refreshToken={refreshToken} SERVER_ADDRESS={SERVER_ADDRESS}/>} />
-                <Route path="/report/2" element={<Report id={2} accessToken={accessToken} setAccessToken={setAccessToken} refreshToken={refreshToken} SERVER_ADDRESS={SERVER_ADDRESS}/>} />
-                <Route path="/report/3" element={<Report id={3} accessToken={accessToken} setAccessToken={setAccessToken} refreshToken={refreshToken} SERVER_ADDRESS={SERVER_ADDRESS}/>} />
-                <Route path="/report/4" element={<Report id={4} accessToken={accessToken} setAccessToken={setAccessToken} refreshToken={refreshToken} SERVER_ADDRESS={SERVER_ADDRESS}/>} />
-                <Route path="/report/5" element={<Report id={5} accessToken={accessToken} setAccessToken={setAccessToken} refreshToken={refreshToken} SERVER_ADDRESS={SERVER_ADDRESS}/>} />
+                <Route path="/report/1" element={<Report id={1} accessToken={accessToken} setAccessToken={setAccessToken} refreshToken={refreshToken} SERVER_ADDRESS={SERVER_ADDRESS} />} />
+                <Route path="/report/2" element={<Report id={2} accessToken={accessToken} setAccessToken={setAccessToken} refreshToken={refreshToken} SERVER_ADDRESS={SERVER_ADDRESS} />} />
+                <Route path="/report/3" element={<Report id={3} accessToken={accessToken} setAccessToken={setAccessToken} refreshToken={refreshToken} SERVER_ADDRESS={SERVER_ADDRESS} />} />
+                <Route path="/report/4" element={<Report id={4} accessToken={accessToken} setAccessToken={setAccessToken} refreshToken={refreshToken} SERVER_ADDRESS={SERVER_ADDRESS} />} />
+                <Route path="/report/5" element={<Report id={5} accessToken={accessToken} setAccessToken={setAccessToken} refreshToken={refreshToken} SERVER_ADDRESS={SERVER_ADDRESS} />} />
             </Routes>
         </div>
     )
