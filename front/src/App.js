8e136d052e6e0b33ac7162df67d3993cd3267a05
Maxwell Babey <babeymax@gmail.com>
2023-04-06 22:49:38 -0700

    Update server url

--- front/src/App.js
+++ front/src/App.js
@@ -3,10 +3,15 @@ import './styles/style.css'
 
 function App() {
 
+  const SERVER_URL = 'https://pokedex-server-z1j1.onrender.com';
+  // const SERVER_URL = 'http://localhost';
+  
+
   const AUTH_PORT = 5011;
-  const SERVER_ADDRESS = `http://localhost:${AUTH_PORT}`;
+  const SERVER_ADDRESS = `${SERVER_URL}:${AUTH_PORT}`;
+  
   const POKE_PORT = 5010;
-  const POKE_ADDRESS = `http://localhost:${POKE_PORT}`;
+  const POKE_ADDRESS = `${SERVER_URL}:${POKE_PORT}`;
 
   return (
     <>
