7198e7aaf2adda8bb7907068a5d3a8bc40f0bde8
Maxwell Babey <babeymax@gmail.com>
2023-04-06 18:36:25 -0700

    Add scaling stat bars

--- front/src/PokemonStats.js
+++ front/src/PokemonStats.js
@@ -17,6 +17,16 @@ function PokemonStats({ selectedPokemon, setSelectedPokemon }) {
         return base["HP"] + base["Attack"] + base["Defense"] + base["Sp. Attack"] + base["Sp. Defense"] + base["Speed"];
     }
 
+    const setStatBarWidth = (stat) => {
+        return (stat / 255) * 100;
+    }
+
+    const setTotalBarWidth = (total) => {
+        return (total / 720) * 100;
+    }
+
+
+
     return (
         selectedPokemon &&
         <div className={"pokemon-stats"}>
@@ -27,13 +37,34 @@ function PokemonStats({ selectedPokemon, setSelectedPokemon }) {
                 <img src={`https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/images/${zeroes(selectedPokemon.id)}${selectedPokemon.id}.png`} alt={`Pokemon #${selectedPokemon.id}: ${selectedPokemon.name.english}`}></img>
             </div>
             <div className={"stats-block"}>
-                <div className={"statname hp"}>HP:</div><div className={"statbar hp"}>{selectedPokemon.base["HP"]}</div>
-                <div className={"statname attack"}>Attack:</div><div className={"statbar attack"}>{selectedPokemon.base["Attack"]}</div>
-                <div className={"statname defense"}>Defense:</div><div className={"statbar defense"}>{selectedPokemon.base["Defense"]}</div>
-                <div className={"statname spattack"}>SpAttack:</div><div className={"statbar spattack"}>{selectedPokemon.base["Sp. Attack"]}</div>
-                <div className={"statname spdefense"}>SpDefense:</div><div className={"statbar spdefense"}>{selectedPokemon.base["Sp. Defense"]}</div>
-                <div className={"statname speed"}>Speed:</div><div className={"statbar speed"}>{selectedPokemon.base["Speed"]}</div>
-                <div className={"statname total"}>Total:</div><div className={"statbar total"}>{calculateTotal(selectedPokemon.base)}</div>
+                <div className={"statname hp"}>HP:</div>
+                <div className={"statbar hp"}
+                    style={{ width: `${setStatBarWidth(selectedPokemon.base["HP"])}%` }}>{selectedPokemon.base["HP"]}</div>
+
+                <div className={"statname attack"}>Attack:</div>
+                <div className={"statbar attack"}
+                    style={{ width: `${setStatBarWidth(selectedPokemon.base["Attack"])}%` }}>{selectedPokemon.base["Attack"]}</div>
+
+                <div className={"statname defense"}>Defense:</div>
+                <div className={"statbar defense"}
+                    style={{ width: `${setStatBarWidth(selectedPokemon.base["Defense"])}%` }}>{selectedPokemon.base["Defense"]}</div>
+
+                <div className={"statname sp-attack"}>Sp. Attack:</div>
+                <div className={"statbar sp-attack"}
+                    style={{ width: `${setStatBarWidth(selectedPokemon.base["Sp. Attack"])}%` }}>{selectedPokemon.base["Sp. Attack"]}</div>
+
+                <div className={"statname sp-defense"}>Sp. Defense:</div>
+                <div className={"statbar sp-defense"}
+                    style={{ width: `${setStatBarWidth(selectedPokemon.base["Sp. Defense"])}%` }}>{selectedPokemon.base["Sp. Defense"]}</div>
+
+                <div className={"statname speed"}>Speed:</div>
+                <div className={"statbar speed"}
+                    style={{ width: `${setStatBarWidth(selectedPokemon.base["Speed"])}%` }}>{selectedPokemon.base["Speed"]}</div>
+
+                <div className={"statname total"}>Total:</div>
+                <div className={"statbar total"}
+                    style={{ width: `${setTotalBarWidth(calculateTotal(selectedPokemon.base))}%` }}>{calculateTotal(selectedPokemon.base)}</div>
+
             </div>
         </div>
     )
